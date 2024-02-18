63e37642c2ff48204dfdc989962100722c478016
JashwanthMallela <JashwanthMallela537@gmail.com>
2024-02-18 13:28:43 +0530

    solved metamask error.

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+
+export const contractAddress = '0x49f88e3B1FAAD1928AbFEC5B46498ed61Da0d3Bb';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-mainnet.g.alchemy.com/v2/g4vM0hgfh0ggHW353NjmsecEX3p1-w_E
 
-/** @type import('hardhat/config').HardhatUserConfig */
+//ff08b72e3a5b3482902b9da8568c32d8687cfcf49144f07e90652c02dd204ecf
+
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.24",
-};
+  solidity : '0.8.19',
+  networks: {
+    ropsten : {
+      url : 'https://eth-sepolia.g.alchemy.com/v2/Bfp1kSAnUtmCqL2Ey8y91Rxs-8XMFlso',
+      accounts : [ 'ff08b72e3a5b3482902b9da8568c32d8687cfcf49144f07e90652c02dd204ecf' ]
+    }
+  }
+}
